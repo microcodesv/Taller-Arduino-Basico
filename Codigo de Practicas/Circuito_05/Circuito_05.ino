c084a49c254800a3fda235f81c769506fb012e14
keikkun <karla.hdz4@gmail.com>
2015-10-17 16:36:33 -0600

    Actualizacion de codigo del taller.

--- Codigo de Practicas/Circuito_05/Circuito_05.ino
+++ Codigo de Practicas/Circuito_05/Circuito_05.ino
@@ -1,23 +1,32 @@
-const int button1Pin = 2; 
-const int button2Pin = 3;  
+const int button1Pin = 3; 
+//const int button2Pin = 3;  
 const int ledPin =  13;    
 
 void setup()
 {
   pinMode(button1Pin, INPUT);
-  pinMode(button2Pin, INPUT);
+//  pinMode(button2Pin, INPUT);
 
   pinMode(ledPin, OUTPUT);      
 }
 
 void loop()
 {
-  int button1State, button2State; 
+  int button1State; 
   
-  button1State = digitalRead(button1Pin);
-  button2State = digitalRead(button2Pin);
+  button1State = digitalRead (button1Pin);
+
+  if ( button1State == LOW )
+  {
+    digitalWrite(ledPin, HIGH);  
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }
+//  button2State = digitalRead(button2Pin);
  
-  if (((button1State == LOW) || (button2State == LOW))
+/*  if (((button1State == LOW) || (button2State == LOW))
       && !                                               
       ((button1State == LOW) && (button2State == LOW))) 
                                                         
@@ -27,5 +36,11 @@ void loop()
   else
   {
     digitalWrite(ledPin, LOW);
-  }
+  }*/
 }
+
+void EncenderLED()
+{
+  digitalWrite(ledPin, HIGH);
+}
+
