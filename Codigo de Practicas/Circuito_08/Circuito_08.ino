651dce526d526c0752a8b9642b7fa5aaf31015f7
Joksan Alvarado <joksan.alvarado@gmail.com>
2016-02-24 16:41:09 -0600

    Mejorada la guia 8 de servomotores - Divididas las rutinas de servo en movimiento directo y continuo - Agregadas 2 funciones con ejemplos mas avanzados del puerto serial

--- Codigo de Practicas/Circuito_08/Circuito_08.ino
+++ Codigo de Practicas/Circuito_08/Circuito_08.ino
@@ -14,11 +14,20 @@ void setup() {
   //Para inicializar un servo se indica el numero de pin al que
   //esta conectado con el metodo "attach"
   servo1.attach(9);
+
+  //Inicializa la terminal serie
+  Serial.begin(9600);
 }
 
 void loop() {
-  int posicion;
+  //Descomente cada una de las rutinas para probarlas
+  //movimientoDirecto();
+  //movimientoContinuo();
+  //controlSerie();
+  anguloSerie();
+}
 
+void movimientoDirecto() {
   //Para establecer la posicion del servo basta con enviar el
   //angulo al que se debe posicionar mediante el metodo write.
   //El servo toma cierta cantidad de tiempo para girar, por lo
@@ -35,6 +44,10 @@ void loop() {
   //Nota: El rango de movimiento de muchos servos va de 0 a 180
   //grados, sin embargo algunos servos tienen menos rango, por
   //lo que se dejan 20 grados como margen de seguridad.
+}
+
+void movimientoContinuo() {
+  int posicion;
 
   //Para mover lentamente el motor se puede hacer un lazo for,
   //el cual incrementa el angulo poco a poco y va intercalando
@@ -52,3 +65,60 @@ void loop() {
   }
 }
 
+void controlSerie() {
+  char dato;
+
+  //Se envian las indicaciones al usuario
+  Serial.println("Seleccione una opcion");
+  Serial.println("1- Posicion A");
+  Serial.println("2- Posicion B");
+  Serial.println("3- Posicion C");
+
+  //La siguiente rutina se repite indefinidamente
+  for (;;) {
+    //Espera a que ingrese un dato por el puerto serie
+    while (!Serial.available());
+
+    //Cuando llega un dato, se lee en la variable
+    dato = Serial.read();
+
+    //Se decide que hacer en caso se reciba un caracter especifico
+    switch (dato) {
+      case '1':
+        servo1.write(20);  //La posicion A es 20 grados
+        break;
+      case '2':
+        servo1.write(90);  //La posicion B es 90 grados
+        break;
+      case '3':
+        servo1.write(160); //La posicion C es 160 grados
+        break;
+    }
+  }
+}
+
+void anguloSerie() {
+  int angulo;
+
+  //Se imprimen las indicaciones
+  Serial.println("Escriba la posicion del servo en grados");
+
+  for (;;) {
+    //Espera a que ingrese un numero por el puerto serie
+    while (!Serial.available());
+
+    //Lee el siguiente numero entero del puerto serie
+    angulo = Serial.parseInt();
+
+    //Recorta el angulo en caso se salga de los valores limites
+    angulo = constrain(angulo, 20, 160);
+
+    //Confirma el angulo ingresado al usuario
+    Serial.print("angulo: ");
+    Serial.print(angulo);
+    Serial.println(" grados");
+
+    //Finalmente establece la posicion del servo
+    servo1.write(angulo);
+  }
+}
